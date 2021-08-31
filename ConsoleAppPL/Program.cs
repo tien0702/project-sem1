4223663a2b593369d29baadb52285af961951019
tien0702 <tientv.0702@gmail.com>
2021-08-31 17:19:56 +0700

    New Update

--- ConsoleAppPL/Program.cs
+++ ConsoleAppPL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Persistance;
 using BL;
 
@@ -8,22 +9,36 @@ namespace ConsoleAppPL
     {
         static void Main(string[] args)
         {
-            Console.Write("User Name: ");
-            string userName = Console.ReadLine();
-            Console.Write("Password: ");
-            string pass = GetPassword();
-            Console.WriteLine();
+            string userName, pass;
+            int login = 0;
+            do{
+                Console.Write("User Name: ");
+                while(!IsValidUserName(userName = Console.ReadLine())) Console.Write("User Name Invalid! Please re-enter: ");
+                Console.Write("Password: ");
+                while(!IsValidPassword(pass = GetPassword())) Console.Write("Password Invalid! Please re-enter: ");
 
-            Cashier cashier = new Cashier(){UserName = userName, Password = pass};
-            CashierBL BL = new CashierBL();
-            int login = BL.Login(cashier);
-            if(login <= 0){
-                Console.WriteLine("Can't Login");
-            }else if(login == 1){
-                Console.WriteLine("Menu Manager");
-            }else if(login == 2){
-                Console.WriteLine("Menu Saleman");
-            }
+                Cashier cashier = new Cashier(){UserName = userName, Password = pass};
+                CashierBL BL = new CashierBL();
+                login = BL.Login(cashier);
+                if(login <= 0){
+                    Console.WriteLine("Wrong account or password!");
+                }else if(login == 1){
+                    Console.WriteLine("Menu Manager");
+                }else if(login == 2){
+                    Console.WriteLine("Menu Saleman");
+                }
+            }while(login <= 0);
+        }
+
+        static bool IsValidUserName(string userName){
+            Regex regex = new Regex(@"^[a-z0-9]{3,16}$");
+            if(regex.IsMatch(userName)) return true;
+            return false;
+        }
+        static bool IsValidPassword(string password){
+            Regex regex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+            if(regex.IsMatch(password)) return true;
+            return false;
         }
         
         static string GetPassword()
@@ -46,6 +61,7 @@ namespace ConsoleAppPL
                     pass += keyInfo.KeyChar;
                 }
             } while (key != ConsoleKey.Enter);
+            Console.WriteLine();
             return pass;
         }
     }
